35263e447f3165b5fc5caf34eccb3eb8ee0c5df7
Alexander Städing <alexanderstaeding@gmail.com>
2023-05-04 10:39:43 +0200

    Initialize template

--- src/graderPublic/java/h04/H04_RubricProvider.java
+++ src/graderPublic/java/h04/H04_RubricProvider.java
@@ -1,12 +1,12 @@
-package h_id_;
+package h04;
 
 import org.sourcegrade.jagr.api.rubric.Rubric;
 import org.sourcegrade.jagr.api.rubric.RubricProvider;
 
-public class H_id__RubricProvider implements RubricProvider {
+public class H04_RubricProvider implements RubricProvider {
 
     public static final Rubric RUBRIC = Rubric.builder()
-        .title("H_id_")
+        .title("H04")
         .build();
 
     @Override
