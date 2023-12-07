8bff972b02841943ca7329ef6c308fa06a3d4a0e
tth05 <36999320+tth05@users.noreply.github.com>
2023-12-08 00:36:24 +0100

    Fix resize condition in `addMainboard`

--- src/main/java/h05/ServerCenter.java
+++ src/main/java/h05/ServerCenter.java
@@ -8,14 +8,15 @@ import h05.model.Mainboard;
 public class ServerCenter implements Configuration {
 
     private int nextFreeSlot = 0;
-    private Mainboard[] mainboards = new Mainboard[0];
+    private Mainboard[] mainboards = new Mainboard[1];
 
     /**
      * Adds a {@link Mainboard} to the server center
+     *
      * @param mainboard specific {@link Mainboard} which gets included
      */
-    public void addMainboard(Mainboard mainboard){
-        if(nextFreeSlot <= mainboards.length){ // currently full -> resize (double size)
+    public void addMainboard(Mainboard mainboard) {
+        if (nextFreeSlot >= mainboards.length) { // currently full -> resize (double size)
             Mainboard[] resizedMainboards = new Mainboard[mainboards.length * 2];
             System.arraycopy(mainboards, 0, resizedMainboards, 0, nextFreeSlot);
             mainboards = resizedMainboards;
@@ -26,7 +27,7 @@ public class ServerCenter implements Configuration {
 
     @Override
     public void rateBy(ComponentRater rater) {
-        for(int i = 0; i < nextFreeSlot; i++){
+        for (int i = 0; i < nextFreeSlot; i++) {
             Mainboard mainboard = mainboards[i];
             mainboard.rateBy(rater);
         }
